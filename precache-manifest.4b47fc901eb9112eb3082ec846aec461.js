33bf0e7966ce36bab8d8b9a8bbc5d7cb8ad974a8
felix1234567890 <felix1234567890@users.noreply.github.com>
2020-04-29 21:10:09 +0000

    deploy: 9e9623f998d18a47272b5202c4fb0c49d137d707

---
+++ precache-manifest.4b47fc901eb9112eb3082ec846aec461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d9589198900c557689f",
+    "url": "/static/css/main.4c64b594.chunk.css"
+  },
+  {
+    "revision": "0d9589198900c557689f",
+    "url": "/static/js/main.0d958919.chunk.js"
+  },
+  {
+    "revision": "bb219dd3c60aaa9b905f",
+    "url": "/static/js/1.bb219dd3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "abd87880bacb5c2dbb365df0f60a6581",
+    "url": "/index.html"
+  }
+];
